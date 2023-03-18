702024fc29179c8821e4ae021abc77f6be9aa06d
ahyeon-github <ahyeon31@sookmyung.ac.kr>
2023-03-18 20:00:32 +0900

    Feat percentage

--- marbon/lib/screens/mainscreen/main_screen.dart
+++ marbon/lib/screens/mainscreen/main_screen.dart
@@ -1,14 +1,52 @@
 import 'package:flutter/material.dart';
-import '../../controller/bottom_navigation_controller.dart';
-import 'package:get/get.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
-class MainScreen extends StatelessWidget {
-  BottomNavigationController bottomNavigationController = Get.find();
-  MainScreen({Key? key}) : super(key: key);
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
 
   @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+
+class _MainScreenState extends State<MainScreen>{
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CircularPercentIndicator(
+                animation: true,
+                animationDuration: 10000,
+                radius: 200,
+                lineWidth: 15,
+                percent: 0.8,
+                progressColor: Color(0xff5C823D),
+                backgroundColor: Color(0xffC4C78C),
+                circularStrokeCap: CircularStrokeCap.round,
+              ),
+              LinearPercentIndicator(
+                animation: true,
+                animationDuration: 10000,
+                lineHeight: 30,
+                percent: 0.8,
+                progressColor: Color(0xff369C33),
+                backgroundColor: Color(0xffD0D0D0),
+              )
+
+
+            ],
+          ),
+        ),
+      ),
     );
+
+
   }
+
 }
